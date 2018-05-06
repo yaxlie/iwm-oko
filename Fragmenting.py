0a00f7068dd4297c9bab8322a10c2c0e22df34a8
yaxlie <m.lesnierowski@gmail.com>
2018-05-07 00:41:58 +0200

    nowe zdjecia do ml

--- Fragmenting.py
+++ Fragmenting.py
@@ -5,13 +5,13 @@ def crop_image(input_image, output_image, start_x, start_y, width, height):
     input_img = Image.open(input_image)
     box = (start_x, start_y, start_x + width, start_y + height)
     output_img = input_img.crop(box)
-    output_img.save("fragments/" + output_image +".jpg")
+    output_img.save("fragments50/" + output_image +".jpg")
 
 def main():
     for i in range(1,3):
-        for j in range(0,12):
-            for k in range(0, 15):
-                crop_image("diabetic_retinopathy/0"+str(i)+"_dr.JPG","dr_"+str(i)+"_"+str(j)+"_"+str(k),
-                           150*k, 150*j, 150, 150)
+        for j in range(20,35):
+            for k in range(20, 40):
+                crop_image("glaucoma/0"+str(i)+"_g.jpg","g_"+str(i)+"_"+str(j)+"_"+str(k),
+                           50*k, 50*j, 50, 50)
 
 if __name__ == '__main__': main()
