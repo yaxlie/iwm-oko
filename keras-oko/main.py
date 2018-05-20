a5f65ba654febea80d5cb2ff419c6a6dfcdcdabd
yaxlie <m.lesnierowski@gmail.com>
2018-05-21 00:04:44 +0200

    optymalizacja

--- keras-oko/main.py
+++ keras-oko/main.py
@@ -8,7 +8,7 @@ import recognize
 
 def classify_img(img):
     # todo zwraca true jesli img jest naczyniem krwionosnym, false wpw.
-    return recognize.CheckImages(np.expand_dims(img, axis=0))[0]
+    return recognize.CheckImages(img)
 
 CHUNK_SIZE = 64
 WIDTH_RESIZED = 876
@@ -16,18 +16,26 @@ HEIGHT_RESIZED = 584
 
 
 img_origin = cv2.imread('./res/02.jpg')
+
+img_origin = cv2.resize(img_origin, (WIDTH_RESIZED, HEIGHT_RESIZED))
+
 height, width, chan = img_origin.shape
 
 img_result = np.zeros([height,width,3],dtype=np.uint8)
 
 # recognize and set new image
 for w in range (0,width-CHUNK_SIZE):
+    images=[]
     for h in range(0, height-CHUNK_SIZE):
-        img_chunk = img_origin[h:h+CHUNK_SIZE, w:w+CHUNK_SIZE]
-        if classify_img(img_chunk):
+        images.append(img_origin[h:h+CHUNK_SIZE, w:w+CHUNK_SIZE])
+        # img_chunk = img_origin[h:h+CHUNK_SIZE, w:w+
+    np_list = np.array([img for img in images])
+    classification = classify_img(np_list)
+    for h in range(0, height - CHUNK_SIZE):
+        if classification[h]:
             img_result[h+int(CHUNK_SIZE/2),w+int(CHUNK_SIZE/2)] = (255, 255, 255)
 #        print("chunk ",w," ",h)
-
+    print(w)
 #     debug
     b = ("Loading [" + "#" * int(w//(width/10)) + " " * (10 - int(w//(width/10))) + "]")
     # \r prints a carriage return first, so `b` is printed on top of the previous line.
